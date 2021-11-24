abdc541709971de55bbaa627273a93353086a4f2
Kishan <84447786+kishannn07@users.noreply.github.com>
2021-11-24 15:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import requests
 from pyrogram import Client as Bot
 
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from callsmusic import run
+from config import API_ID, API_HASH, BOT_TOKEN, SUDO_USERS
 
-response = requests.get(BG_IMAGE)
-file = open("./etc/foreground.png", "wb")
-file.write(response.content)
-file.close()
 
 bot = Bot(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="modules"),
+    plugins=dict(root="handlers")
 )
+
+print(f"[INFO]: KISHU MUSIC VERSION 2.0 STARTED !")
 
 bot.start()
 run()
