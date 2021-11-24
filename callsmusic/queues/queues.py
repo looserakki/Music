d42b28b5f78f7672a98e8089f2173b5a9b0072c3
Kishan <84447786+kishannn07@users.noreply.github.com>
2021-11-24 17:22:38 +0530

    Update queues.py

--- callsmusic/queues/queues.py
+++ callsmusic/queues/queues.py
@@ -1,30 +1,5 @@
-# kishumusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-from asyncio import Queue as _Queue
-from asyncio import QueueEmpty as Empty
-from typing import Dict
-
-
-class Queue(_Queue):
-    _queue: list = []
-
-    def clear(self):
-        self._queue.clear()
-
+from asyncio import Queue, QueueEmpty as Empty
+from typing import Dict, Union
 
 queues: Dict[int, Queue] = {}
 
@@ -36,13 +11,12 @@ async def put(chat_id: int, **kwargs) -> int:
     return queues[chat_id].qsize()
 
 
-def get(chat_id: int) -> Dict[str, str]:
+def get(chat_id: int) -> Union[Dict[str, str], None]:
     if chat_id in queues:
         try:
             return queues[chat_id].get_nowait()
         except Empty:
-            return {}
-    return {}
+            return None
 
 
 def is_empty(chat_id: int) -> bool:
@@ -64,5 +38,5 @@ def clear(chat_id: int):
         if queues[chat_id].empty():
             raise Empty
         else:
-            queues[chat_id].clear()
+            queues[chat_id].queue = []
     raise Empty
